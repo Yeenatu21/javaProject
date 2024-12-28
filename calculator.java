4cd7f0fabe914a8331ddb9d1940cb2cc10512c88
Kibrom <gdeykibrom8@gmail.com>
2024-12-28 11:15:08 +0300

    my first simple calculator code in java

---
+++ calculator.java
@@ -0,0 +1,10 @@
+package javaProject;
+
+public class calculator{
+    public static void main(String[] args){
+        int x=5;
+        int y=8;
+        int sum=x+y;
+        System.out.println(sum);
+    }
+}
